bdeae5e1bb68a39f740ddcaefc048d7f4a2556f1
Leonardo Ceschin <leonardofcoceschin@gmail.com>
2018-03-19 16:26:21 -0300

    Adicionado dado

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 import youtube_dl
+import random
+#import music
 
 client = discord.Client()
-
-player = 
+#musicPlayer = Music(client)
 
 @client.event
 async def on_ready():
@@ -14,19 +15,40 @@ async def on_message(message):
     if message.content.lower().startswith('!shop'):
         await client.send_message(message.channel, "Shopping não!!!")
 
+    if message.content.lower().startswith('!roll'):
+        dice = random.randint(1, 6)
+        resposta = ""
+        if dice == 1:
+            resposta = "Voce tirou {}!".format(dice)
+        elif dice == 2:
+            resposta = "Voce tirou {}!".format(dice)    
+        elif dice == 3:
+            resposta = "Voce tirou {}!".format(dice)
+        elif dice == 4:
+            resposta = "Voce tirou {}!".format(dice)
+        elif dice == 5:
+            resposta = "Voce tirou {}!".format(dice)
+        else:
+            resposta = "Voce tirou {}!".format(dice)
+
+        await client.send_message(message.channel, resposta)
+
+    #if message.content.lower().startswith('!music'):
+    #    msgArray = []
+    #    msgArray = message.content.split()
+    #    musicPlayer.entra_canal().toca_musica(msgArray[1])
+
     if message.content.lower().startswith('!music'):
         msgArray = []
         msgArray = message.content.split()
         voice = await client.join_voice_channel(client.get_channel('425083604721336324'))
         player = await voice.create_ytdl_player(msgArray[1])
-        player.start()
+    player.start()
 
 @client.event
 async def on_member_join(member):
     channelId = client.get_channel("425083604721336322")
     msg = "Alá! o {} chegou!".format(member.mention)
     await client.send_message(channelId, msg)
-
-    
 
 client.run('NDI1MTExNDIzMzI5NTAxMjE1.DZCrcA.e-OZAY-6rvHa3TQP9TmF6ss7N_4')
--- music.py
+++ music.py
@@ -1,11 +1,19 @@
 import discord
 import youtube_dl
+import asyncio
 
 class MusicPlayer():
-    def __init__(self, discord.Client()):
-        self.client = discord.Client()
-
-    def tocaMusica():
+    def __init__(self, client:discord.Client):
+        self.client = client
         
-            
-    def paraMusica():    
+    async def entra_canal(self):
+        canalId = client.get_channel('425083604721336324')
+        self.voice = await client.join_voice_channel(canalId)
+    
+    async def toca_musica(self, string):
+        player = await self.voice.create_ytdl_player(string)
+        player.start()
+              
+    async def para_musica():
+        player = await self.voice.create_ytdl_player(string)
+        player.stop()    
