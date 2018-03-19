a9a6432dc4fce7e6cbbe9d27451443ce099e748e
Leonardo Ceschin <leonardofcoceschin@gmail.com>
2018-03-18 23:55:06 -0300

    Adicionado mensagem de welcome.

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ async def on_ready():
 @cliente.event
 async def on_message(message):
     if message.content.lower().startswith('!shop'):
-        await cliente.send_message(message.channel, "Shopping não!!!")    
+        await cliente.send_message(message.channel, "Shopping não!!!")
 
+@cliente.event
+async def on_member_join(member):
+    channelId = cliente.get_channel("425083604721336322")
+    msg = "Alá! o {} chegou!".format(member.mention)
+    await cliente.send_message(channelId, msg)
 
 cliente.run('NDI1MTExNDIzMzI5NTAxMjE1.DZCrcA.e-OZAY-6rvHa3TQP9TmF6ss7N_4')
